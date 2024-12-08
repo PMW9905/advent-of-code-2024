ad6d6befe5bcbfe246ebea5f1cefaf34fdc7061a
PMW9905 <pw9905college@gmail.com>
2024-12-07 21:16:47 -0600

    completed problem 1 and 2 of day 1

--- parker/day_1/problem2/src/main.rs
+++ parker/day_1/problem2/src/main.rs
@@ -1,5 +1,6 @@
 use input_read_util::read_file_return_buffer;
 use std::env;
+use std::collections::HashMap;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -21,7 +22,7 @@ fn main() {
     }; 
 
     let mut left_nums: Vec<i32> = Vec::new();
-    let mut right_nums: Vec<i32> = Vec::new();
+    let mut right_nums_map: HashMap<i32,i32> = HashMap::new();
 
     for line in input {
         let mut line_whitespace = line.split_whitespace(); 
@@ -29,16 +30,25 @@ fn main() {
         let right_num: i32 = line_whitespace.next().unwrap_or_default().parse().expect("Unable to parse right of {line}");
 
         left_nums.push(left_num);
-        right_nums.push(right_num);
-    }
 
-    left_nums.sort();
-    right_nums.sort();
+        let prev_value: i32 = match right_nums_map.get(&right_num) {
+            Some(value) => value.clone(),
+            None => 0
+        };
+
+        right_nums_map.insert(right_num, prev_value+1);
+    }
 
     let mut dif: i32 = 0;
 
-    for (a, b) in left_nums.iter().zip(right_nums.iter()) {
-        dif += (a - b).abs();
+    for num in left_nums.iter() {
+                
+        let times_seen: i32 = match right_nums_map.get(&num) {
+            Some(value) => value.clone(),
+            None => 0
+        };
+
+        dif += num * times_seen;
     }
 
     println!("{}",dif);
