9a5815325f62555dc2dcbd41511e5440fb5e5c79
PMW9905 <pw9905college@gmail.com>
2024-12-08 16:30:44 -0600

    hngggg

--- parker/day_2/problem2/src/main.rs
+++ parker/day_2/problem2/src/main.rs
@@ -3,9 +3,69 @@ use std::env;
 
 const MAX_DIF: i32 = 3;
 
-fn is_level_valid(level: &Vec<i32>) -> bool {
+fn does_level_match_rules(should_increase: &bool, prev_level: &i32, cur_level: &i32) -> bool {
+    if *prev_level == *cur_level {
+        return false;
+    }
+
+    if (*prev_level < *cur_level) != *should_increase {
+        return false;
+    }
+
+    if (*prev_level - *cur_level).abs() > MAX_DIF {
+        return false;
+    }
 
     return true;
+}
+
+fn is_report_safe(report: &Vec<i32>) -> bool {
+    if report.len() <= 1 {
+        return true;
+    }
+
+    for should_increase in vec![true, false] {
+        let mut bad_levels = 0;
+        let mut ignore_current_level = false;
+
+        for i in 1..report.len() {
+
+            if ignore_current_level {
+                ignore_current_level = false;
+                continue;
+            }
+
+            if !does_level_match_rules(&should_increase, &report[i - 1], &report[i]) {
+                bad_levels += 1;
+                if bad_levels > 1 || i + 1 >= report.len() {
+                    break;
+                }
+
+                let should_remove_prev = !does_level_match_rules(&should_increase, &report[i - 1], &report[i + 1]);
+                let should_remove_cur = !does_level_match_rules(&should_increase, &report[i], &report[i + 1]);
+
+                if should_remove_prev && should_remove_cur {
+                    bad_levels += 1;
+                    break;
+                }
+
+                if should_remove_prev {
+                    continue; 
+                }
+
+                if should_remove_cur {
+                    ignore_current_level = true;
+                    continue;
+                }
+            }
+        }
+
+        if bad_levels <= 1 {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 fn main() {
@@ -30,28 +90,14 @@ fn main() {
     let mut num_success: i32 = 0;
 
     for line in input {
-        let level: Vec<i32> = line.split_whitespace().filter_map(|s| s.parse().ok()).collect();
-        if is_level_valid(&level) {
-            num_success+=1;
+        let report: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|s| s.parse().ok())
+            .collect();
+        if is_report_safe(&report) {
+            num_success += 1;
         }
     }
 
-    println!("{}", num_success);
+    println!("{}!", num_success);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
