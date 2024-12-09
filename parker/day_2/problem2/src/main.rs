fa7597f3c1c6172126d57814340371c803bc935c
PMW9905 <pw9905college@gmail.com>
2024-12-08 21:36:09 -0600

    oh my god it was a backwards greater than symbol

--- parker/day_2/problem2/src/main.rs
+++ parker/day_2/problem2/src/main.rs
@@ -4,6 +4,9 @@ use std::env;
 const MAX_DIF: i32 = 3;
 
 fn does_level_match_rules(should_increase: &bool, prev_level: &i32, cur_level: &i32) -> bool {
+
+    println!("{} {} {} {}", *prev_level, *cur_level, prev_level, cur_level);
+
     if *prev_level == *cur_level {
         return false;
     }
@@ -24,47 +27,48 @@ fn is_report_safe(report: &Vec<i32>) -> bool {
         return true;
     }
 
-    for should_increase in vec![true, false] {
-        let mut bad_levels = 0;
-        let mut ignore_current_level = false;
 
+    for should_increase in vec![true, false] {
+        let mut errors_found: u32 = 0;
+        let mut skip_this_level = false;
         for i in 1..report.len() {
 
-            if ignore_current_level {
-                ignore_current_level = false;
+            if skip_this_level {
+                skip_this_level = false;
                 continue;
             }
 
             if !does_level_match_rules(&should_increase, &report[i - 1], &report[i]) {
-                bad_levels += 1;
-                if bad_levels > 1 || i + 1 >= report.len() {
+                println!("{}",i);
+                errors_found += 1;
+
+                if errors_found > 1 || i + 1 == report.len() {
                     break;
-                }
+                } 
 
-                let should_remove_prev = !does_level_match_rules(&should_increase, &report[i - 1], &report[i + 1]);
-                let should_remove_cur = !does_level_match_rules(&should_increase, &report[i], &report[i + 1]);
+                let prev_can_be_removed = i-1 == 0 || does_level_match_rules(&should_increase, &report[i-2], &report[i]); 
+                let cur_can_be_removed = i+1 >= report.len() || does_level_match_rules(&should_increase, &report[i-1], &report[i+1]); 
+                println!("prev remove? : {}, cur remove? : {}",prev_can_be_removed,cur_can_be_removed);
 
-                if should_remove_prev && should_remove_cur {
-                    bad_levels += 1;
-                    break;
-                }
-
-                if should_remove_prev {
-                    continue; 
-                }
-
-                if should_remove_cur {
-                    ignore_current_level = true;
+                if cur_can_be_removed {
+                    skip_this_level = true;
                     continue;
+                }
+
+                if prev_can_be_removed {
+                    continue;
+                }
+
+                if !prev_can_be_removed && !cur_can_be_removed {
+                    errors_found+=1;
+                    break;
                 }
             }
         }
-
-        if bad_levels <= 1 {
+        if errors_found <= 1 {
             return true;
         }
     }
-
     return false;
 }
 
@@ -94,8 +98,10 @@ fn main() {
             .split_whitespace()
             .filter_map(|s| s.parse().ok())
             .collect();
+
         if is_report_safe(&report) {
             num_success += 1;
+            println!("{:?}", &report);
         }
     }
 
